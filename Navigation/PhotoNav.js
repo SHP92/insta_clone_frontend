efad77a77252d62982ba4e25421caa445ec53b6c
SHP92 <sunliebsh@naver.com>
2020-04-03 22:30:49 +0900

    select photos

--- Navigation/PhotoNav.js
+++ Navigation/PhotoNav.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import SelectPhoto from '../Screens/Photo/SelectPhoto';
 import TakePhoto from '../Screens/Photo/TakePhoto';
 import UploadPhoto from '../Screens/Photo/UploadPhoto';
 import { NavigationContainer } from '@react-navigation/native';
+import theme from '../theme';
+import { Ionicons } from '@expo/vector-icons';
 
 
 export default function PhotoNav() {
@@ -15,9 +17,35 @@ export default function PhotoNav() {
   const PhotoTab= () => {
       return(
         <NavigationContainer independent={true}>
-          <Tab.Navigator tabBarPosition='bottom'>
-              <Tab.Screen name="SelectPhoto" component={SelectPhoto} />
-              <Tab.Screen name="TakePhoto" component={TakePhoto} />
+          <Tab.Navigator 
+            tabBarPosition='bottom' 
+            tabBarOptions={{
+              indicatorStyle:{backgroundColor:'white'},
+              tabStyle:{backgroundColor:theme.backgruondGreyColor},
+            }}
+          >
+              <Tab.Screen 
+                name="SelectPhoto" component={SelectPhoto} 
+                options={{
+                  tabBarLabel:({focused, color})=>(
+                    <Text style={{
+                      fontWeight: focused ? 'bold': null,
+                      color: focused ? color : theme.darkGreyColor,
+                    }}>SELECT</Text>
+                  )
+                }}
+              />
+              <Tab.Screen 
+                name="TakePhoto" component={TakePhoto} 
+                options={{
+                  tabBarLabel:({focused, color})=>(
+                    <Text style={{
+                      fontWeight: focused ? 'bold': null,
+                      color: focused ? color : theme.darkGreyColor,
+                    }}>TAKE</Text>
+                  )
+                }}
+              />
           </Tab.Navigator>
         </NavigationContainer>
       )
@@ -25,8 +53,9 @@ export default function PhotoNav() {
   
   return (
     <Stack.Navigator>
-      <Stack.Screen name="PhotoTab" component={PhotoTab}/>
-      <Stack.Screen name="UploadPhoto" component={UploadPhoto} />
+      <Stack.Screen name="PhotoTab" component={PhotoTab} options={{
+        headerTitle:()=><Ionicons name='ios-camera' size={30}/>
+      }}/>
     </Stack.Navigator>
   );
 }
--- Screens/Photo/SelectPhoto.js
+++ Screens/Photo/SelectPhoto.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, FlatList, Dimensions, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+import Loader from '../../Components/Loader';
 
-export default function SelectPhoto({ navigation }){
+export default function SelectPhoto(){
+    const [loading, setLoading] = useState(true);
+    const [hasPermission, setHasPermission] = useState(false);
+    const [allPhotos, setAllPhotos] = useState();
+    const [selected, setSelected] = useState('');
+
+    const getPhotos = async() => {
+        try {
+            const photos = await MediaLibrary.getAssetsAsync();
+            setAllPhotos(photos.assets);
+            setSelected(allPhotos[0]);
+        } catch (e) {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
+    };
+    const askPermission = async() => {
+        try{
+            const permissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            if (permissions.status === 'granted') {
+                setHasPermission(true);
+                getPhotos();
+            }
+        } catch (e) {
+            console.log(e);
+            setHasPermission(false);
+        }
+    };
+
+    useEffect(()=>{
+        askPermission();
+    }, []);
+
     return(
         <View style={styles.container}>
-            <TouchableOpacity onPressOut={() => navigation.navigate('UploadPhoto')}>
-                <Text> SelectPhoto </Text>
-            </TouchableOpacity>
+            {loading ? <Loader /> : hasPermission ? 
+            <ScrollView>
+                <Image source={{uri: selected.uri ?? allPhotos[0].uri}} style={styles.wholeImage}/>
+                <FlatList
+                    data={allPhotos}
+                    renderItem={({item})=>(
+                        <TouchableOpacity onPressOut={()=>setSelected(item)}>
+                            <Image 
+                                source={{uri: item.uri}} 
+                                style={{...styles.image, opacity: item.id === selected.id ? 0.4 : 1.0 }}
+                            />
+                        </TouchableOpacity>
+                    )}
+                    numColumns={3}
+                /> 
+            </ScrollView>
+            : null}
         </View>
     )
 };
@@ -16,5 +67,14 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    image: {
+        width: Dimensions.get('screen').width /3,
+        height: Dimensions.get('screen').height /4,
+    },
+    wholeImage: {
+        width: Dimensions.get('screen').width,
+        height: Dimensions.get('screen').height /2,
+        resizeMode: 'cover',
     },
   });
