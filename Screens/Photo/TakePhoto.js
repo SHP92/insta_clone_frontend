2872d0949ed2a4e7cde92fb5d1047f8048c543d5
SHP92 <sunliebsh@naver.com>
2020-04-04 10:32:21 +0900

    save photos

--- Screens/Photo/TakePhoto.js
+++ Screens/Photo/TakePhoto.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 import { Camera } from 'expo-camera';
 import Loader from '../../Components/Loader';
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import theme from '../../theme';
 
 export default function TakePhoto(){
     const [loading, setLoading] = useState(true);
     const [hasPermission, setHasPermission] = useState(false);
     const [type, setType] = useState(Camera.Constants.Type.back);
+    const [canTakePhoto, setCanTakePhoto] = useState(true);
+    const cameraRef = useRef();
 
     const askPermission = async() => {
         try{
@@ -23,6 +27,18 @@ export default function TakePhoto(){
             setLoading(false);
         }
     };
+    const takePhotos = async() => {
+        if (!canTakePhoto) return
+
+        try {
+            const photo = await cameraRef.current.takePictureAsync({});
+            await MediaLibrary.createAssetAsync(photo.uri);
+            setCanTakePhoto(false);
+        } catch (e) {
+            console.log(e);
+            setCanTakePhoto(true);
+        }
+    };
 
     useEffect(()=>{
         askPermission();
@@ -31,10 +47,10 @@ export default function TakePhoto(){
     return(
         loading ? <Loader/> : hasPermission ? 
         <View style={{ flex: 1 }}>
-            <Camera style={{ flex: 1 }} type={type}>
+            <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
                 <View style={{flex: 1, backgroundColor: 'transparent', flexDirection: 'row'}}>
                     <TouchableOpacity
-                        style={{alignSelf: 'flex-end', alignItems: 'center'}}
+                        style={{alignSelf: 'flex-start', alignItems: 'center'}}
                         onPress={() => {
                             setType(
                             type === Camera.Constants.Type.back
@@ -46,6 +62,9 @@ export default function TakePhoto(){
                         <MaterialIcons name='switch-camera' size={30} style={{padding:20, color:'white'}}/>
                     </TouchableOpacity>
                 </View>
+                <TouchableOpacity style={styles.buttonContainer} onPressOut={takePhotos} disabled={!canTakePhoto}>
+                    <MaterialCommunityIcons name='circle-slice-8' color='white' size={75}/>
+                </TouchableOpacity>
             </Camera>
         </View> : null
     )
@@ -56,5 +75,9 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        padding: 10,
     },
   });
